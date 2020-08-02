657f16c0dec78c954e5160e91b689a5ed52cdb87
Lapb <lapb@syneforge.com>
2020-08-02 11:44:07 +0300

    can works in this way

--- src/main/scala/check/NotWorksOnFMain.scala
+++ src/main/scala/check/NotWorksOnFMain.scala
@@ -1,7 +1,9 @@
 package check
 
+import java.time.LocalTime
+
 import cats.Monad
-import cats.effect.{Blocker, Bracket, ExitCode, IO, IOApp, Resource}
+import cats.effect.{Blocker, Bracket, ExitCode, IO, IOApp, Resource, Sync}
 import distage.{Injector, ModuleDef, Roots}
 import izumi.distage.model.Locator
 import izumi.distage.model.definition.DIResource
@@ -31,16 +33,19 @@ object NotWorksOnFMain extends IOApp {
     new DBComponent[IO].resource
   }
 
-  case class DBComponent[F[_]: Monad]() {
+  case class DBComponent[F[_]: Sync]() {
+    val M = implicitly[Sync[F]]
     import cats.implicits._
     def resource: Resource[F, (ApplicationDB, Blocker)] = {
       for {
         db <- Resource.make(
           for {
-            _  <- println("init app db").pure[F]
-            db <- ApplicationDB("appDB").pure[F]
+            _  <- M.delay(println(s"${LocalTime.now} init app db 1"))
+            _  <- M.delay(println(s"${LocalTime.now} init app db 2"))
+            _  <- M.delay(println(s"${LocalTime.now} init app db 3"))
+            db <- M.delay(ApplicationDB("appDB"))
           } yield db
-        )(_ => println("close app db").pure[F])
+        )(_ => M.delay(println(s"${LocalTime.now} close app db")))
       } yield (db, Blocker.liftExecutionContext(scala.concurrent.ExecutionContext.Implicits.global))
     }
   }
